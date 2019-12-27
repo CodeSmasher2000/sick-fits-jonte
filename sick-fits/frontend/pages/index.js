2b34b9c50455aa58cd73cc2efe2c456e3e45556a
Jonatan Wahlstedt <jonatan.wahlstedt@younium.com>
2019-12-27 19:17:56 +0100

    started intro, added basic css, connected to prisma

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+const Home = props => (
+	<div>
+		<Link href='/sell'>
+			<a>Sell!</a>
+		</Link>
+		<p>hej!</p>
+	</div>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,7 @@
+const Sell = props => (
+	<div>
+		<p>SELLL!!</p>
+	</div>
+);
+
+export default Sell;
